7a0c4ae5a078e0e7d606cb0fbb0adbf7a853ae1f
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2024-09-15 11:33:56 +0200

    Add Item doc

--- lib/Data/InfoBox/Item.pm
+++ lib/Data/InfoBox/Item.pm
@@ -48,3 +48,164 @@ sub BUILD {
 1;
 
 __END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Data::InfoBox::Item - Data object for info box item.
+
+=head1 SYNOPSIS
+
+ use Data::InfoBox;
+
+ my $obj = Data::InfoBox::Item->new(%params);
+ my $icon_url = $obj->icon_url;
+ my $icon_char = $obj->icon_char;
+ my $text = $obj->text;
+ my $url = $obj->url;
+
+=head1 METHODS
+
+=head2 C<new>
+
+ my $obj = Data::InfoBox->new(%params);
+
+Constructor.
+
+Returns instance of object.
+
+=over 8
+
+=item * C<icon_url>
+
+Icon URL.
+
+It's optional.
+
+=item * C<icon_char>
+
+Icon character. Could be UTF-8 character. Only one character.
+
+It's optional.
+
+=item * C<text>
+
+Item text. Must me a L<Data::Text::Simple> object.
+
+It's required.
+
+=item * C<url>
+
+URL of item.
+
+It's optional.
+
+=back
+
+=head2 C<icon_url>
+
+ my $icon_url = $obj->icon_url;
+
+Get icon URL.
+
+Returns string.
+
+=head2 C<icon_char>
+
+ my $icon_char = $obj->icon_char;
+
+Get icon character.
+
+Returns string.
+
+=head2 C<text>
+
+ my $text = $obj->text;
+
+Get text of item.
+
+Returns L<Data::Text::Simple> object.
+
+=head2 C<url>
+
+ my $url = $obj->url;
+
+Get URL of item.
+
+Returns string.
+
+=head1 ERRORS
+
+ new():
+         From Mo::utils:
+                 Parameter 'icon_char' has length greater than '1'.
+                         Value: %s
+                 Parameter 'text' is required.
+                 Parameter 'text' must be a 'Data::Text::Simple' object.
+                         Value: %s
+                         Reference: %s
+                 
+         From Mo::utils::URI:
+                 Parameter 'icon_url' doesn't contain valid location.
+                         Value: %s
+                 Parameter 'url' doesn't contain valid location.
+                         Value: %s
+
+=head1 EXAMPLE
+
+=for comment filename=create_infobox_item_object_and_print.pl
+
+ use strict;
+ use warnings;
+
+ use Data::InfoBox::Item;
+ use Data::Text::Simple;
+
+ my $obj = Data::InfoBox::Item->new(
+         'icon_url' => 'https://example.com/foo.png',
+         'text' => Data::Text::Simple->new(
+                 'text' => 'Funny item'
+         ),
+         'url' => 'https://skim.cz',
+ );
+
+ # Print out.
+ print "Icon URL: ".$obj->icon_url."\n";
+ print "Text: ".$obj->text->text."\n";
+ print "URL: ".$obj->url."\n";
+
+ # Output:
+ # Icon URL: https://example.com/foo.png
+ # Text: Funny item
+ # URL: https://skim.cz
+
+=head1 DEPENDENCIES
+
+L<Mo>,
+L<Mo::utils>,
+L<Mo::utils::URI>.
+
+=head1 REPOSITORY
+
+L<https://github.com/michal-josef-spacek/Data-InfoBox>
+
+=head1 AUTHOR
+
+Michal Josef Špaček L<mailto:skim@cpan.org>
+
+L<http://skim.cz>
+
+=head1 LICENSE AND COPYRIGHT
+
+© 2024 Michal Josef Špaček
+
+BSD 2-Clause License
+
+=head1 VERSION
+
+0.01
+
+=cut
