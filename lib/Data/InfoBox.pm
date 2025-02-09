5c9ea3f2b2e10866766beee11ccf5557a0b6dab5
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2025-02-09 17:09:26 +0100

    Prepare for 0.04 version

--- lib/Data/InfoBox.pm
+++ lib/Data/InfoBox.pm
@@ -6,7 +6,7 @@ use warnings;
 use Mo qw(build is);
 use Mo::utils 0.21 qw(check_array_object check_array_required);
 
-our $VERSION = 0.03;
+our $VERSION = 0.04;
 
 has items => (
 	is => 'ro',
@@ -149,6 +149,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.03
+0.04
 
 =cut
--- lib/Data/InfoBox/Item.pm
+++ lib/Data/InfoBox/Item.pm
@@ -7,7 +7,7 @@ use Mo qw(build is);
 use Mo::utils 0.08 qw(check_isa check_length check_required);
 use Mo::utils::URI 0.02 qw(check_location check_uri);
 
-our $VERSION = 0.03;
+our $VERSION = 0.04;
 
 has icon => (
 	is => 'ro',
@@ -254,6 +254,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.03
+0.04
 
 =cut
